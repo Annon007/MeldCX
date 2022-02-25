8f94966c6a723fbf99830bbaf57602d162312d0b
Annon007 <62867777+Annon007@users.noreply.github.com>
2022-02-25 22:38:05 +0600

    circles border and shadow added

--- src/components/circles/circles.js
+++ src/components/circles/circles.js
@@ -9,10 +9,12 @@ const Circles = props => {
         width: "4rem",
         height: "4rem",
         top: generateRandomNumber(1, 80),
-        left: generateRandomNumber(1, 90),
+        left: generateRandomNumber(1, 95),
         borderRadius: "50%",
         backgroundColor: "#fff",
         transition: ` all 0.2s ease-out`,
+        border: "1px solid #ff7043",
+        boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"
     }
     return <div style={styles}>
 
